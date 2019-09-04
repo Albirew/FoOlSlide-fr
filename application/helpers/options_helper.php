b9056d17d1cacca1e68a63815fecd34c338de4f9
Albirew <rad1cal_edward@yahoo.fr>
2019-09-04 21:19:31 +0200

    upgraded gravatar to https

--- application/helpers/options_helper.php
+++ application/helpers/options_helper.php
@@ -139,7 +139,7 @@ function current_url_real()
  */
 function get_gravatar($email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array())
 {
-	$url = 'http://www.gravatar.com/avatar/';
+	$url = 'https://www.gravatar.com/avatar/';
 	$url .= md5(strtolower(trim($email)));
 	$url .= "?s=$s&d=$d&r=$r";
 	if ($img)
